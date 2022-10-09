0af454585becefbcf44914c7e7ba2f762b9a7fbe
Fernando Mottin Maschmann <fernando.maschmann@outlook.com>
2022-10-09 18:30:21 -0300

    better sql

--- backend/controllers/locations.js
+++ backend/controllers/locations.js
@@ -2,9 +2,10 @@ const db = require("./db_connection");
 
 async function getLocations(req, res) {
   const row = await db.query(
-    `SELECT Lat, Lon, CONCAT(Street, ", ", AdressNumber) 'Adress'
+    `SELECT Lat, Lon, CONCAT(u.FirstName, " ", u.LastName) 'UserName'
+            ,CONCAT(Street, ", ", AdressNumber) 'Adress'
             ,(SELECT COUNT(*) FROM Installations WHERE UserId = a.UserId) 'Instalations'
-      FROM Adresses a;`
+        FROM Adresses a JOIN Users u ON a.UserId = u.Id;`
   );
   res.send(row);
 }
